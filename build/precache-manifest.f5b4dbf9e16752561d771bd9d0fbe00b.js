2b9b51e4aa41e1f74dbecfd2d6a8de57fc6ba60c
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2020-04-04 13:16:20 +0300

    folder /build to git

---
+++ build/precache-manifest.f5b4dbf9e16752561d771bd9d0fbe00b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e167238bf08f7bff4cba374540e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca8737c91aad4cfe7b9",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e88360be524989a8140e",
+    "url": "/static/css/main.f545f0ab.chunk.css"
+  },
+  {
+    "revision": "6ca8737c91aad4cfe7b9",
+    "url": "/static/js/2.ce21f58c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce21f58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88360be524989a8140e",
+    "url": "/static/js/main.6dd8f630.chunk.js"
+  },
+  {
+    "revision": "53ee946cd185b61e8805",
+    "url": "/static/js/runtime-main.48263512.js"
+  },
+  {
+    "revision": "508b0489471ef9146c99bc1e17bc2486",
+    "url": "/static/media/bg-map.508b0489.jpg"
+  },
+  {
+    "revision": "c86d98c4392864c95f3325b174e45d3a",
+    "url": "/static/media/bg.c86d98c4.jpg"
+  }
+]);
