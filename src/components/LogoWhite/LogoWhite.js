9866abe9071ad83b53413902c2b81a4e36adf409
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2020-04-11 11:44:10 +0300

    some refactoring, added logo, made ref MapboxGl

---
+++ src/components/LogoWhite/LogoWhite.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './Logo.css';
+import logo from '../../img/logo.svg'
+
+const Logo = () => <img className="logo" src={logo} alt="logo" />;
+
+export default Logo;
---
+++ src/components/MapBoxGl/MapBoxGl.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import './Subtitle.css';
+
+export default class Subtitle extends Component {
+  
+  onChangeForm = (subtitleName) => {
+    subtitleName === 'Sign Up' ?
+        this.props.changePage('sign') :
+        this.props.changePage('login')
+  };
+  
+  render() {
+    const {subtitle, subtitleLink} = this.props;
+    
+    return (
+        <div className='subtitle-block'>
+          <h5 className='subtitle'>{subtitle}</h5>
+          <h5 onClick={() => this.onChangeForm(subtitleLink)}
+              className='subtitle-link'>{subtitleLink}</h5>
+        </div>
+    
+    )
+    
+  }
+  
+};
+
